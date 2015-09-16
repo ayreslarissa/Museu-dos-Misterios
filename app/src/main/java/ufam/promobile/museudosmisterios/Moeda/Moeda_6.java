771c559ba01b172c558ea4d24982723a8faf9e7b
Larissa Ayres <larimayres@gmail.com>
2015-09-16 13:23:00 -0400

    Enviando novas activities: Moeda_6, Passaro_32, Passaro_34

--- app/src/main/java/ufam/promobile/museudosmisterios/Moeda/Moeda_6.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Moeda/Moeda_6.java
@@ -1,11 +1,19 @@
 package ufam.promobile.museudosmisterios.Moeda;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
+import ufam.promobile.museudosmisterios.Passaro.Passaro_32;
+import ufam.promobile.museudosmisterios.Passaro.Passaro_34;
 import ufam.promobile.museudosmisterios.R;
+import ufam.promobile.museudosmisterios.Sandalia.Sandalia_28;
 
 public class Moeda_6 extends ActionBarActivity {
 
@@ -22,6 +30,44 @@ public class Moeda_6 extends ActionBarActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        final Button proximoMoeda6 = (Button) findViewById(R.id.button_moeda_6_proximo);
+        final Button anteriorMoeda6 = (Button) findViewById(R.id.button_moeda_6_anterior);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        proximoMoeda6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                alertDialogBuilder.setTitle("Qual o número da caixa que está a pista?");
+                alertDialogBuilder
+                        .setMessage("")
+                        .setCancelable(true)
+                        .setPositiveButton("Caixa IV", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Moeda_6.this, Passaro_32.class);
+                                startActivityForResult(i, 1);
+                            }
+                        })
+                        .setNegativeButton("Caixa VI", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Moeda_6.this, Passaro_34.class);
+                                startActivityForResult(i, 1);
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+            }
+        });
+
+        anteriorMoeda6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (Moeda_6.this, Sandalia_28.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/ufam/promobile/museudosmisterios/Passaro/Passaro_32.java
@@ -0,0 +1,26 @@
+package ufam.promobile.museudosmisterios.Passaro;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.WindowManager;
+
+import ufam.promobile.museudosmisterios.R;
+
+public class Passaro_32 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passaro_32);
+        // Orientaçao Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
