7d81ea03a574f3ae817c68d8bd20ef6fd35d5fde
Larissa Ayres <larimayres@gmail.com>
2015-09-17 13:44:26 -0400

    Enviando novas activities: Moeda_16, Moeda_36 e Esfinge_8

---
+++ app/src/main/java/ufam/promobile/museudosmisterios/Moeda/Moeda_16.java
@@ -0,0 +1,70 @@
+package ufam.promobile.museudosmisterios.Moeda;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import ufam.promobile.museudosmisterios.Esfinge.Esfinge_8;
+import ufam.promobile.museudosmisterios.R;
+import ufam.promobile.museudosmisterios.Sandalia.Sandalia_28;
+
+public class Moeda_16 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moeda_16);
+
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        final Button proximoMoeda16 = (Button) findViewById(R.id.button_moeda_16_proximo);
+        final Button anteriorMoeda16 = (Button) findViewById(R.id.button_moeda_16_anterior);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        proximoMoeda16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                alertDialogBuilder.setTitle("Qual número falta?");
+                alertDialogBuilder
+                        .setMessage("")
+                        .setCancelable(true)
+                        .setPositiveButton("Número 8", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Moeda_16.this, Moeda_36.class);
+                                startActivityForResult(i, 1);
+                            }
+                        })
+                        .setNegativeButton("Número 9", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Moeda_16.this, Esfinge_8.class);
+                                startActivityForResult(i, 1);
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+            }
+        });
+
+        anteriorMoeda16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Moeda_16.this, Sandalia_28.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
--- app/src/main/java/ufam/promobile/museudosmisterios/Passaro/Passaro_34.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Passaro/Passaro_34.java
@@ -1,10 +1,15 @@
 package ufam.promobile.museudosmisterios.Passaro;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
+import ufam.promobile.museudosmisterios.Moeda.Moeda_16;
+import ufam.promobile.museudosmisterios.Moeda.Moeda_6;
 import ufam.promobile.museudosmisterios.R;
 
 public class Passaro_34 extends ActionBarActivity {
@@ -20,6 +25,27 @@ public class Passaro_34 extends ActionBarActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        final Button proximopassaro34 = (Button) findViewById(R.id.button_passaro_34_proximo);
+        final Button anteriorpassaro34 = (Button) findViewById(R.id.button_passaro_34_anterior);
+
+        //PASSA PARA A PÁGINA MOEDA_16
+        proximopassaro34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Passaro_34.this, Moeda_16.class);
+                startActivity(intent);
+            }
+        });
+
+        //VOLTA PARA A PÁGINA MOEDA_6
+        anteriorpassaro34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Passaro_34.this, Moeda_6.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
