3c2f2a146df3974008df37c895a9480732c8f2ac
Larissa Ayres <larimayres@gmail.com>
2015-09-18 14:07:57 -0400

    Enviando as activities Esfinge_8, Moeda_36 e Sandalia_20 (Esse último é específico para tablet pois a imagem para telas abaixo de 7.0" ficou mto grande. Esperar imagem definitiva feita pela designer)

--- app/src/main/java/ufam/promobile/museudosmisterios/Esfinge/Esfinge_8.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Esfinge/Esfinge_8.java
@@ -1,10 +1,15 @@
 package ufam.promobile.museudosmisterios.Esfinge;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
+import ufam.promobile.museudosmisterios.Moeda.Moeda_16;
 import ufam.promobile.museudosmisterios.R;
 
 public class Esfinge_8 extends ActionBarActivity {
@@ -13,27 +18,46 @@ public class Esfinge_8 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esfinge_8);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_esfinge_8, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Orientação Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        final TextView texto = (TextView) findViewById(R.id.textViewTexto_esfinge_8_1);
+        final Button proximoEsfinge8_1 = (Button) findViewById(R.id.button_esfinge_8_proximo1);
+        final Button proximoEsfinge8_2 = (Button) findViewById(R.id.button_esfinge_8_proximo2);
+        final Button anteriorEsfinge8 = (Button) findViewById(R.id.button_esfinge_8_anterior);
+
+        texto.setText(R.string.esfinge_8_1);
+
+        //Botão para passar o texto
+        proximoEsfinge8_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.esfinge_8_2);
+                proximoEsfinge8_1.setVisibility(View.INVISIBLE);
+                proximoEsfinge8_2.setVisibility(View.VISIBLE);
+            };
+        });
+
+        //Botão para passar o texto
+        proximoEsfinge8_2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.esfinge_8_3);
+                proximoEsfinge8_2.setVisibility(View.INVISIBLE);
+
+            };
+        });
+
+        anteriorEsfinge8.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Esfinge_8.this, Moeda_16.class);
+                startActivity(intent);
+            };
+        });
     }
+
 }
--- app/src/main/java/ufam/promobile/museudosmisterios/Moeda/Moeda_36.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Moeda/Moeda_36.java
@@ -1,11 +1,16 @@
 package ufam.promobile.museudosmisterios.Moeda;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import ufam.promobile.museudosmisterios.R;
+import ufam.promobile.museudosmisterios.Sandalia.Sandalia_20;
 
 public class Moeda_36 extends ActionBarActivity {
 
@@ -13,27 +18,57 @@ public class Moeda_36 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moeda_36);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_moeda_36, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Orientação Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        final TextView texto = (TextView) findViewById(R.id.textViewTexto_moeda_36_1);
+        final Button proximoMoeda36_1 = (Button) findViewById(R.id.button_moeda_36_proximo_1);
+        final Button proximoMoeda36_2 = (Button) findViewById(R.id.button_moeda_36_proximo_2);
+        final Button proximoMoeda36_3 = (Button) findViewById(R.id.button_moeda_36_proximo_3);
+        final Button anteriorMoeda36 = (Button) findViewById(R.id.button_moeda_36_anterior);
+
+        texto.setText(R.string.moeda_36_1);
+
+        //Botão para passar o texto
+        proximoMoeda36_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.moeda_36_2);
+                proximoMoeda36_1.setVisibility(View.INVISIBLE);
+                proximoMoeda36_2.setVisibility(View.VISIBLE);
+            };
+        });
+
+        //Botão para passar o texto
+        proximoMoeda36_2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.moeda_36_3);
+                proximoMoeda36_2.setVisibility(View.INVISIBLE);
+                proximoMoeda36_3.setVisibility(View.VISIBLE);
+            };
+        });
+
+
+        proximoMoeda36_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Moeda_36.this, Sandalia_20.class);
+                startActivity(intent);
+            }
+        });
+
+        anteriorMoeda36.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Moeda_36.this, Moeda_16.class);
+                startActivity(intent);
+            };
+        });
+
     }
+
 }
