1e3494698d1d13e0aabf975660736b2fa8296acf
Larissa Ayres <larimayres@gmail.com>
2015-09-30 17:53:26 -0400

    Sandalia_15

--- app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_15.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_15.java
@@ -1,10 +1,17 @@
 package ufam.promobile.museudosmisterios.Sandalia;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import ufam.promobile.museudosmisterios.Esfinge.Esfinge_22;
+import ufam.promobile.museudosmisterios.Esfinge.Esfinge_5;
 import ufam.promobile.museudosmisterios.R;
 
 public class Sandalia_15 extends ActionBarActivity {
@@ -13,27 +20,55 @@ public class Sandalia_15 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sandalia_15);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sandalia_15, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Orientação Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        final TextView texto = (TextView) findViewById(R.id.textView_sandalia_15_1);
+        final ImageView imagem1 = (ImageView) findViewById(R.id.imageView_sandalia_15_1);
+        final ImageView imagem2 = (ImageView) findViewById(R.id.imageView_sandalia_15_2);
+        final Button proximoSandalia15_1 = (Button) findViewById(R.id.button_sandalia_15_proximo1);
+        final Button proximoSandalia15_2 = (Button) findViewById(R.id.button_sandalia_15_proximo2);
+        final Button anteriorSandalia15 = (Button) findViewById(R.id.button_sandalia_15_anterior);
+
+
+        texto.setText(R.string.sandalia_15_1);
+
+        //Botão para passar o texto
+        proximoSandalia15_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_15_2);
+                proximoSandalia15_1.setVisibility(View.INVISIBLE);
+                proximoSandalia15_2.setVisibility(View.VISIBLE);
+                imagem1.setVisibility(View.VISIBLE);
+                imagem2.setVisibility(View.VISIBLE);
+
+            };
+        });
+
+        //Botão para passar o texto
+        proximoSandalia15_2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Sandalia_15.this, Esfinge_22.class);
+                startActivity(intent);
+
+            };
+        });
+
+        anteriorSandalia15.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Sandalia_15.this, Esfinge_5.class);
+                startActivity(intent);
+            }
+
+            ;
+        });
+
     }
+
 }
--- app/src/main/java/ufam/promobile/museudosmisterios/Vaso/Vaso_37.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Vaso/Vaso_37.java
@@ -1,10 +1,14 @@
 package ufam.promobile.museudosmisterios.Vaso;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
+import ufam.promobile.museudosmisterios.Esfinge.Esfinge_5;
 import ufam.promobile.museudosmisterios.R;
 
 public class Vaso_37 extends ActionBarActivity {
@@ -13,27 +17,22 @@ public class Vaso_37 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaso_37);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_vaso_37, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Orientação Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        final Button anteriorvaso37 = (Button) findViewById(R.id.button_vaso_37_anterior);
+        anteriorvaso37.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Vaso_37.this, Esfinge_5.class);
+                startActivity(intent);
+            }
 
-        return super.onOptionsItemSelected(item);
+
+        });
     }
+
 }
