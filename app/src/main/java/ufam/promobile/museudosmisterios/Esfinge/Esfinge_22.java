25381a25defc1b194a6374fb82661046efaa7279
Larissa Ayres <larimayres@gmail.com>
2015-10-01 14:27:18 -0400

    Esfinge_22, Sandalia_35

--- app/src/main/java/ufam/promobile/museudosmisterios/Esfinge/Esfinge_22.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Esfinge/Esfinge_22.java
@@ -1,39 +1,68 @@
 package ufam.promobile.museudosmisterios.Esfinge;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import ufam.promobile.museudosmisterios.R;
+import ufam.promobile.museudosmisterios.Sandalia.Sandalia_15;
+import ufam.promobile.museudosmisterios.Sandalia.Sandalia_35;
 
-public class Esfinge_22 extends ActionBarActivity {
+public class Esfinge_22 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esfinge_22);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_esfinge_22, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Orientação Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        final TextView texto = (TextView) findViewById(R.id.textView_esfinge_22_1);
+        final Button proximoEsfinge22_1 = (Button) findViewById(R.id.button_esfinge_22_proximo1);
+        final Button proximoEsfinge22_2 = (Button) findViewById(R.id.button_esfinge_22_proximo2);
+        final Button anteriorEsfinge22_1 = (Button) findViewById(R.id.button_esfinge_22_anterior);
+
+
+        texto.setText(R.string.esfinge_22_1);
+
+        //Botão para passar o texto
+        proximoEsfinge22_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.esfinge_22_2);
+                proximoEsfinge22_1.setVisibility(View.INVISIBLE);
+                proximoEsfinge22_2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //Botão para passar o texto
+        proximoEsfinge22_2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Esfinge_22.this, Sandalia_35.class);
+                startActivity(intent);
+
+            };
+        });
+
+        anteriorEsfinge22_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Esfinge_22.this, Sandalia_15.class);
+                startActivity(intent);
+            }
+
+            ;
+        });
+
     }
+
 }
