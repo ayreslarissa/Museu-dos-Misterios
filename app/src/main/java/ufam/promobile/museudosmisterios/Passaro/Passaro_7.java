97f3d4583e9d7792e41757cb4490ae5ff0a23696
Larissa Ayres <larimayres@gmail.com>
2015-10-07 14:43:54 -0400

    Enviando Passaro_7 e suas duas imagens

--- app/src/main/java/ufam/promobile/museudosmisterios/Passaro/Passaro_7.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Passaro/Passaro_7.java
@@ -1,10 +1,16 @@
 package ufam.promobile.museudosmisterios.Passaro;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import ufam.promobile.museudosmisterios.Moeda.Moeda_33;
 import ufam.promobile.museudosmisterios.R;
 
 public class Passaro_7 extends ActionBarActivity {
@@ -13,27 +19,54 @@ public class Passaro_7 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passaro_7);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_passaro_7, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Orientaçao Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        final Button anteriorPassaro7 = (Button) findViewById(R.id.button_passaro_7_anterior);
+        final TextView texto = (TextView) findViewById(R.id.textView_passaro_7_1);
+        final Button proximo1Passaro7 = (Button) findViewById(R.id.button_passaro_7_proximo1);
+        final Button proximo2Passaro7 = (Button) findViewById(R.id.button_passaro_7_proximo2);
+        final ImageView imagem_armadura = (ImageView) findViewById(R.id.imageView_passaro_7_1);
+        final ImageView imagem_escudo = (ImageView) findViewById(R.id.imageView_passaro_7_2);
+
+        anteriorPassaro7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText(R.string.passaro_7_1);
+                proximo1Passaro7.setVisibility(View.VISIBLE);
+                proximo2Passaro7.setVisibility(View.INVISIBLE);
+                anteriorPassaro7.setVisibility(View.INVISIBLE);
+                imagem_armadura.setVisibility(View.VISIBLE);
+                imagem_escudo.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        proximo1Passaro7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText(R.string.capacete_14_2);
+                proximo1Passaro7.setVisibility(View.INVISIBLE);
+                proximo2Passaro7.setVisibility(View.VISIBLE);
+                anteriorPassaro7.setVisibility(View.VISIBLE);
+                imagem_armadura.setVisibility(View.INVISIBLE);
+                imagem_escudo.setVisibility(View.VISIBLE);
+            }
+        });
+
+        proximo2Passaro7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Passaro_7.this, Moeda_33.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
