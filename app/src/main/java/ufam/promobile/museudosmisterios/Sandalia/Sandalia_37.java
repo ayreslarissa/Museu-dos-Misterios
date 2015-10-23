9a87caae2e22f38426b6045331cf4807ca785615
Larissa Ayres <larimayres@gmail.com>
2015-10-23 14:43:03 -0400

    Enviando sandalia_37

--- app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_37.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_37.java
@@ -1,9 +1,13 @@
 package ufam.promobile.museudosmisterios.Sandalia;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import ufam.promobile.museudosmisterios.R;
 
@@ -22,6 +26,67 @@ public class Sandalia_37 extends ActionBarActivity {
             e.printStackTrace();
         }
 
+        final TextView texto = (TextView) findViewById(R.id.textView_sandalia_37);
+        final Button proximo1 = (Button) findViewById(R.id.button_sandalia_37_proximo1);
+        final Button proximo2 = (Button) findViewById(R.id.button_sandalia_37_proximo2);
+        final Button anterior1 = (Button) findViewById(R.id.button_sandalia_37_anterior1);
+        final Button anterior2 = (Button) findViewById(R.id.button_sandalia_37_anterior2);
+        final Button anterior3 = (Button) findViewById(R.id.button_sandalia_37_anterior3);
+
+        texto.setText(R.string.sandalia_37_1);
+
+        proximo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_37_2);
+                proximo1.setVisibility(View.INVISIBLE);
+                proximo2.setVisibility(View.VISIBLE);
+                anterior1.setVisibility(View.INVISIBLE);
+                anterior2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        proximo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_37_3);
+                proximo2.setVisibility(View.INVISIBLE);
+                anterior2.setVisibility(View.INVISIBLE);
+                anterior3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        anterior1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Sandalia_37.this, Sandalia_23.class);
+                startActivity(intent);
+            }
+        });
+
+        anterior2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_37_1);
+                proximo1.setVisibility(View.VISIBLE);
+                proximo2.setVisibility(View.INVISIBLE);
+                anterior1.setVisibility(View.VISIBLE);
+                anterior2.setVisibility(View.INVISIBLE);
+                anterior3.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        anterior3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_37_2);
+                proximo1.setVisibility(View.INVISIBLE);
+                proximo2.setVisibility(View.VISIBLE);
+                anterior1.setVisibility(View.INVISIBLE);
+                anterior2.setVisibility(View.VISIBLE);
+                anterior3.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
 }
