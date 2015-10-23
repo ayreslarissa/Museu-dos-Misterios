774a271e8a1ebe0381a85929652dad21ff78c874
Larissa Ayres <larimayres@gmail.com>
2015-10-23 13:57:15 -0400

    Sandalia_23 e Sandalia_41 completos

--- app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_23.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_23.java
@@ -1,39 +1,99 @@
 package ufam.promobile.museudosmisterios.Sandalia;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import ufam.promobile.museudosmisterios.R;
+import ufam.promobile.museudosmisterios.Vaso.Vaso_9;
 
-public class Sandalia_23 extends ActionBarActivity {
+public class Sandalia_23 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sandalia_23);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sandalia_23, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Orientaçao Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        final TextView texto = (TextView) findViewById(R.id.textViewTexto_sandalia_23_1);
+        final Button proximoSandalia_23_1 = (Button) findViewById(R.id.button_sandalia_23_proximo1);
+        final Button proximoSandalia_23_2 = (Button) findViewById(R.id.button_sandalia_23_proximo2);
+        final Button proximoSandalia_23_3 = (Button) findViewById(R.id.button_sandalia_23_proximo3);
+        final Button anteriorSandalia_23_1 = (Button) findViewById(R.id.button_sandalia_23_anterior);
+        final Button anteriorSandalia_23_2 = (Button) findViewById(R.id.button_sandalia_23_anterior2);
+        final ImageView imagem1 = (ImageView) findViewById(R.id.imageView_sandalia_23_1);
+        final ImageView imagem2 = (ImageView) findViewById(R.id.imageView_sandalia_23_2);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        texto.setText(R.string.sandalia_23_1);
+
+        //Botão para passar o texto
+        proximoSandalia_23_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_23_2);
+                proximoSandalia_23_1.setVisibility(View.INVISIBLE);
+                proximoSandalia_23_2.setVisibility(View.VISIBLE);
+
+                imagem1.setVisibility(View.INVISIBLE);
+                imagem2.setVisibility(View.VISIBLE);
+            };
+        });
+
+        //Botão para passar o texto
+        proximoSandalia_23_2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_23_3);
+                proximoSandalia_23_2.setVisibility(View.INVISIBLE);
+                proximoSandalia_23_3.setVisibility(View.VISIBLE);
+
+                imagem2.setVisibility(View.INVISIBLE);
+
+            };
+        });
+
+        proximoSandalia_23_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                alertDialogBuilder.setTitle("Qual a quantidade de contas?");
+                alertDialogBuilder
+                        .setMessage("")
+                        .setCancelable(true)
+                        .setPositiveButton("222", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Sandalia_23.this, Sandalia_37.class);
+                                startActivityForResult(i, 1);
+                            }
+                        })
+                        .setNegativeButton("242", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Sandalia_23.this, Vaso_9.class);
+                                startActivityForResult(i, 1);
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+            }
+        });
+
+
     }
+
+
 }
--- app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_41.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_41.java
@@ -1,39 +1,76 @@
 package ufam.promobile.museudosmisterios.Sandalia;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
+import ufam.promobile.museudosmisterios.Passaro.Passaro_12;
 import ufam.promobile.museudosmisterios.R;
 
-public class Sandalia_41 extends ActionBarActivity {
+public class Sandalia_41 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sandalia_41);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sandalia_41, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Orientaçao Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        final TextView texto = (TextView) findViewById(R.id.textViewTexto_sandalia_41_1);
+        final Button proximoSandalia_41_1 = (Button) findViewById(R.id.button_sandalia_41_proximo1);
+        final Button proximoSandalia_41_2 = (Button) findViewById(R.id.button_sandalia_41_proximo2);
+        final Button anteriorSandalia_41 = (Button) findViewById(R.id.button_sandalia_41_anterior);
+        final Button anteriorSandalia2_41 = (Button) findViewById(R.id.button_sandalia_41_anterior2);
+
+        texto.setText(R.string.sandalia_41_1);
+
+        //Botão para passar o texto
+        proximoSandalia_41_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_41_2);
+                proximoSandalia_41_1.setVisibility(View.INVISIBLE);
+                proximoSandalia_41_2.setVisibility(View.VISIBLE);
+            };
+        });
+
+        //Botão para passar o texto
+        proximoSandalia_41_2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_41_3);
+                proximoSandalia_41_2.setVisibility(View.INVISIBLE);
+
+            };
+        });
+
+        anteriorSandalia_41.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Sandalia_41.this, Passaro_12.class);
+                startActivity(intent);
+            };
+        });
+
+        anteriorSandalia2_41.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_41_2);
+                proximoSandalia_41_1.setVisibility(View.INVISIBLE);
+                proximoSandalia_41_2.setVisibility(View.VISIBLE);
+                anteriorSandalia_41.setVisibility(View.VISIBLE);
+                anteriorSandalia2_41.setVisibility(View.INVISIBLE);
+            };
+        });
     }
+
 }
+
+
