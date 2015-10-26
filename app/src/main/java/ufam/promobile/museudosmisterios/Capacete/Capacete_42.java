9e8d8bc32ffbbb5185cb5b3f29ff58a4b86d8e52
Larissa Ayres <larimayres@gmail.com>
2015-10-26 17:12:25 -0400

    Capacete_42, Capacete_5

--- app/src/main/java/ufam/promobile/museudosmisterios/Capacete/Capacete_42.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Capacete/Capacete_42.java
@@ -1,7 +1,9 @@
 package ufam.promobile.museudosmisterios.Capacete;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.WindowManager;
 
 import ufam.promobile.museudosmisterios.R;
 
@@ -11,6 +13,15 @@ public class Capacete_42 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capacete_42);
+
+        // Orientaçao Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- app/src/main/java/ufam/promobile/museudosmisterios/Capacete/Capacete_5.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Capacete/Capacete_5.java
@@ -1,16 +1,41 @@
 package ufam.promobile.museudosmisterios.Capacete;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import ufam.promobile.museudosmisterios.R;
 
-public class Capacete_5 extends ActionBarActivity {
+public class Capacete_5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capacete_5);
+
+        // Orientaçao Paisagem (Landscape)
+        try {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        final Button proximo1 = (Button) findViewById(R.id.button_capacete_5_proximo1);
+
+        proximo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Capacete_5.this, Capacete_13.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 }
