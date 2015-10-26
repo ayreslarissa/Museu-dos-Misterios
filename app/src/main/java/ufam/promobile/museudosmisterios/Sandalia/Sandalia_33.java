4c18110c4cd6b27928f18122fd4a8b94c9227a00
Larissa Ayres <larimayres@gmail.com>
2015-10-26 14:15:49 -0400

    Sandalia_33

--- app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_33.java
+++ app/src/main/java/ufam/promobile/museudosmisterios/Sandalia/Sandalia_33.java
@@ -1,10 +1,15 @@
 package ufam.promobile.museudosmisterios.Sandalia;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
+import ufam.promobile.museudosmisterios.Esfinge.Esfinge_17;
 import ufam.promobile.museudosmisterios.R;
 
 public class Sandalia_33 extends ActionBarActivity {
@@ -21,6 +26,40 @@ public class Sandalia_33 extends ActionBarActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        final TextView texto = (TextView) findViewById(R.id.textView_sandalia_33);
+        final Button proximo = (Button) findViewById(R.id.button_sandalia_33_proximo1);
+        final Button anterior1 = (Button) findViewById(R.id.button_sandalia_33_anterior1);
+        final Button anterior2 = (Button) findViewById(R.id.button_sandalia_33_anterior2);
+
+        texto.setText(R.string.sandalia_33_1);
+
+        anterior1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Sandalia_33.this, Esfinge_17.class);
+                startActivity(intent);
+            }
+        });
+        proximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_33_2);
+                anterior1.setVisibility(View.INVISIBLE);
+                anterior2.setVisibility(View.VISIBLE);
+                proximo.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        anterior2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texto.setText(R.string.sandalia_33_1);
+                anterior1.setVisibility(View.VISIBLE);
+                anterior2.setVisibility(View.INVISIBLE);
+                proximo.setVisibility(View.VISIBLE);
+            }
+        });
 
     }
 
